69e7901670679a22d9eda1f751cd8fd3d2a0f39e
a756177020 <271377321@qq.com>
2018-02-27 10:13:26 +0800

    准备阶段

--- TuSouSou/ViewController.swift
+++ TuSouSou/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let manager = AFHTTPManager.manager
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
